b30330d6c1426b27ddeb19fb1caed9e68d01d86e
Qais Patankar <qaisjp@gmail.com>
2018-01-20 22:56:21 +0000

    add spawning

--- game/server.lua
+++ game/server.lua
@@ -3,6 +3,17 @@ local gamestate = {
     wifiObjects = {},
 }
 
+local spawnpoints = {
+    {2460.83496, -1635.53442, 200.08281},
+    {2459.38159, -1635.71252, 200.08281},
+    {2457.32324, -1636.10168, 200.08281},
+    {2454.28906, -1636.53101, 200.09010}
+}
+local spawnIndex = 1
+
+local skins = {29, 194}
+local skinIndex = 1
+
 addEventHandler('onResourceStart', resourceRoot, function()
     -- Load wifiObjects
     for i=1, 4 do
@@ -18,4 +29,16 @@ end)
 addEvent('hc:onPlayerReady', true)
 addEventHandler('hc:onPlayerReady', root, function()
     outputChatBox('Welcome to Hackathon Simulator Cambridge.', source)
+    spawnPlayer(source, spawnpoints[spawnIndex], 180, skins[skinIndex])
+
+    spawnIndex = spawnIndex + 1
+    skinIndex = skinIndex + 1
+
+    if spawnIndex >= #spawnpoints then
+        spawnIndex = 1
+    end
+
+    if skinIndex >= #skins then
+        skinIndex = 1
+    end
 end)
