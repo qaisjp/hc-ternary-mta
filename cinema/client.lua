05eab4acb6a492c4faf95f9bdf74731766befae7
Qais Patankar <qaisjp@gmail.com>
2018-01-20 21:57:15 +0000

    trying to get cinema to work

--- cinema/client.lua
+++ cinema/client.lua
@@ -5,9 +5,6 @@ function render()
 	local x, y, z = 2464.24561, -1645.59924, 230.33594
 	dxDrawMaterialLine3D(x, y, z, x, y, z-9, browser, 18.2, tocolor(255, 255, 255, 255), 2458.06006, -1645.59766, 230.33594)
 	dxDrawImage(0, 0, sw, sh, browser, 0, 0, 0, tocolor(255,255,255,255), true)
-	if localPlayer.name == 'qaisjp' then
-		outputDebugString('render')
-	end
 end
 
 addEventHandler('onClientBrowserCreated', browser, function()
