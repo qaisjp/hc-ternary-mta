f5c6cbf43f91128e95b355bf55f6741682c5567f
pecoraa <elena.l@hotmail.lt>
2018-01-20 20:40:33 +0000

    test out with 2 skins

--- skins/client.lua
+++ skins/client.lua
@@ -1,15 +1,27 @@
--- local skins = {
---     {29, 'wmydrug.txd'}
--- }
+modified_skins = {
+    [29] = 'wmydrug.txd',
+    [194] = 'crogrl3.txd'
+}
+
 function onStart()
     outputDebugString("Loading skins..")
-    for _, skin in ipairs(skins) do
-    local filename = 'crogrl3.txd'
-    local id = 194
-    local txd = engineLoadTXD(filename)
-    if not engineImportTXD(txd, id) then
-        outputDebugString("Importing ID " .. tostring(id) .. " failed")
+
+    for k, v in modified_skins do
+        local txd = engineLoadTXD(v)
+        local success = engineImportTXD(txd, k)
+        if not success then
+            outputDebugString("Importing ID " .. tostring(k) .. " failed")
+        end
+
     end
+
+
+    -- local filename = 'crogrl3.txd'
+    -- local id = 194
+    -- local txd = engineLoadTXD(filename)
+    -- if not engineImportTXD(txd, id) then
+    --     outputDebugString("Importing ID " .. tostring(id) .. " failed")
+    -- end
 end
 
 addEventHandler('onClientResourceStart', resourceRoot, onStart)
