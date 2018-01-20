2ad818c9014d27f147748821419d4df0b6645da7
Qais Patankar <qaisjp@gmail.com>
2018-01-20 22:49:32 +0000

    .

--- game/server.lua
+++ game/server.lua
@@ -1,3 +1,20 @@
+local gamestate = {
+    wifiStates = {true, true, true, true},
+    wifiObjects = {},
+}
+
+addEventHandler('onResourceStart', resourceRoot, function()
+    -- Load wifiObjects
+    for i=1, 4 do
+        local wifi = getElementByID("router" .. tostring(i))
+        if not wifi then
+            return outputDebugString('Could not find router' .. tostring(i))
+        end
+
+        wifiObjects[i] = wifi
+    end
+end)
+
 addEvent('hc:onPlayerReady', true)
 addEventHandler('hc:onPlayerReady', root, function()
     outputChatBox('Welcome to Hackathon Simulator Cambridge.', source)
