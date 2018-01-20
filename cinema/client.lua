8af17f4a1a5d606aa7fdefadfc6cea88288ddc7a
Qais Patankar <qaisjp@gmail.com>
2018-01-20 21:44:37 +0000

    test image

--- cinema/client.lua
+++ cinema/client.lua
@@ -4,6 +4,7 @@ local browser = createBrowser(sw, sh, false, true)
 function render()
 	local x, y, z = 2464.24561, -1645.59924, 230.33594
 	dxDrawMaterialLine3D(x, y, z, x, y, z-9, browser, 18.2, tocolor(255, 255, 255, 255), 2458.06006, -1645.59766, 230.33594)
+	dxDrawImage(0, 0, sw, sh, browser, 0, 0, 0, tocolor(255,255,255,255), true)
 end
 
 addEventHandler('onClientBrowserCreated', browser, function()
