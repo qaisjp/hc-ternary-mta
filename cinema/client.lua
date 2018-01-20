acb862fdbbd31463c90409d67eea402dc0844b29
Qais Patankar <qaisjp@gmail.com>
2018-01-20 22:11:21 +0000

    maybe cinema will work now

--- cinema/client.lua
+++ cinema/client.lua
@@ -5,7 +5,7 @@ outputChatBox('Cinema loaded.')
 function render()
 	local x, y, z = 2464.24561, -1645.59924, 230.33594
 	dxDrawMaterialLine3D(x, y, z, x, y, z-9, browser, 18.2, tocolor(255, 255, 255, 255), 2458.06006, -1645.59766, 230.33594)
-	dxDrawImage(0, 0, sw, sh, browser, 0, 0, 0, tocolor(255,255,255,255), true)
+	-- dxDrawImage(0, 0, sw, sh, browser, 0, 0, 0, tocolor(255,255,255,255), true)
 end
 
 function loadCinemaURL()
