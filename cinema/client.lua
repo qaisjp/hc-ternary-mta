d7957747ae4d9d2327db395d675f9d1f92d9be69
Qais Patankar <qaisjp@gmail.com>
2018-01-20 21:46:36 +0000

    trying to get cinema to work

--- cinema/client.lua
+++ cinema/client.lua
@@ -1,6 +1,6 @@
 local sw, sh = guiGetScreenSize() 
 local browser = createBrowser(sw, sh, false, true)
-
+outputChatBox('Cinema loaded.')
 function render()
 	local x, y, z = 2464.24561, -1645.59924, 230.33594
 	dxDrawMaterialLine3D(x, y, z, x, y, z-9, browser, 18.2, tocolor(255, 255, 255, 255), 2458.06006, -1645.59766, 230.33594)
