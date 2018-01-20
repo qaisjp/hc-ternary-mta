ace2033518c0e2228e3408ddfcecf3e9f4ec3e8b
pecoraa <elena.l@hotmail.lt>
2018-01-20 19:32:34 +0000

    Add first skin

---
+++ skins/client.lua
@@ -0,0 +1,10 @@
+function onStart()
+    local filename = 'wmydrug.txd'
+    local id = 29
+    local txd = engineLoadTXD(filename)
+    if not engineImportTXD(txd, id) then
+        outputDebugString("Importing ID " .. tostring(id) .. " failed")
+    end
+end
+
+addEventHandler('onClientResourceStart', resourceRoot, onStart)
