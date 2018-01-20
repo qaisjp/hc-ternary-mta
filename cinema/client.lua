6966580e8918eb9fd07b33ed437efd9576dda941
Qais Patankar <qaisjp@gmail.com>
2018-01-20 21:55:05 +0000

    trying to get cinema to work

--- cinema/client.lua
+++ cinema/client.lua
@@ -6,7 +6,7 @@ function render()
 	dxDrawMaterialLine3D(x, y, z, x, y, z-9, browser, 18.2, tocolor(255, 255, 255, 255), 2458.06006, -1645.59766, 230.33594)
 	dxDrawImage(0, 0, sw, sh, browser, 0, 0, 0, tocolor(255,255,255,255), true)
 end
-addEventHandler('onClientPreRender', root, render)
+
 addEventHandler('onClientBrowserCreated', browser, function()
 	outputChatBox("Browser loaded.")
 	loadBrowserURL(browser, 'https://hackcambridge.com/live')
