129a6da6a58292c2879896104d7f87889f797388
Qais Patankar <qaisjp@gmail.com>
2018-01-20 22:26:48 +0000

    .

--- game/server.lua
+++ game/server.lua
@@ -1,4 +1,4 @@
 addEvent('hc:onPlayerReady', true)
 addEventHandler('hc:onPlayerReady', root, function()
-    outputChatBox('Welcome to Hackathon Simulator Cambridge.')
+    outputChatBox('Welcome to Hackathon Simulator Cambridge.', source)
 end)
