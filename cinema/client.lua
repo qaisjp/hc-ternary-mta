15ff865a456368bdf88309b540e7a506c37af48b
Justin Glibert <me@justinglibert.com>
2018-01-21 08:49:20 +0000

    hh

--- cinema/client.lua
+++ cinema/client.lua
@@ -4,7 +4,7 @@ local browser
 outputChatBox('Cinema loaded.')
 function render()
 	local x, y, z = 2464.24561, -1645.59924, 230.33594
-	dxDrawMaterialLine3D(x, y, z, x, y, z+4, browser, 18.2, tocolor(255, 255, 255, 255), 2458.06006, -1645.59766, 230.33594+4)
+	dxDrawMaterialLine3D(x, y, z + 4, x, y, z, browser, 18.2, tocolor(255, 255, 255, 255), 2458.06006, -1645.59766, 230.33594+4)
 	-- dxDrawImage(0, 0, sw, sh, browser, 0, 0, 0, tocolor(255,255,255,255), true)
 end
 
