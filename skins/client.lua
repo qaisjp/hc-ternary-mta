3d44564625f9afde6c2680574b1d68905cd14f65
pecoraa <elena.l@hotmail.lt>
2018-01-21 00:39:50 +0000

    add all characters

--- skins/client.lua
+++ skins/client.lua
@@ -1,6 +1,9 @@
 modified_skins = {
     [29] = 'wmydrug.txd',
-    [194] = 'crogrl3.txd'
+    [194] = 'crogrl3.txd',
+    [46] = 'hmyri.txd',
+    [233] = 'swfyst.txd',
+    [250] = 'swmycr.txd'
 }
 
 function onStart()
