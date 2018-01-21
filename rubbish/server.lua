e0e1601419c04bcc9f3a52ac31ebc12b67daf874
Justin Glibert <me@justinglibert.com>
2018-01-21 02:14:13 +0000

    test rubbish

--- rubbish/server.lua
+++ rubbish/server.lua
@@ -1,18 +1,19 @@
 local rubbish = {
 
 }
-local xStart = 0
-local yStart = 0
-local xEnd = 0
-local yEnd = 0
-local z = 0
+local xStart = 2410.84326
+local yStart = -1628.72656
+local xEnd = 2451.45044
+local yEnd = -1665.77454
+local z = 228.42049
 
 function spawnRubbish()
     local x = math.random(xStart, xEnd)
     local y = math.random(yStart, yEnd)
     createPickup(x, y ,z, 3, 2674)
 end
-
+-- {2451.45044, -1628.72656, 228.42049}
+--  {2410.84326, -1665.77454, 228.42049}
 addEventHandler('onResourceStart', resourceRoot, function()
     setTimer(function()
         spawnRubbish()
