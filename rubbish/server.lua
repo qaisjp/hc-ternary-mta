2eaa2211ae7cbde1039fd356a3bfe4e0aeeca695
Justin Glibert <me@justinglibert.com>
2018-01-21 02:47:16 +0000

    test rub

--- rubbish/server.lua
+++ rubbish/server.lua
@@ -1,6 +1,10 @@
 local rubbish = {
 
 }
+local activeRubbish = 0
+
+treshold = 7
+
 local xStart = 2410.84326
 local yStart = 1628.72656
 local xEnd = 2451.45044
@@ -10,14 +14,27 @@ local z = 228.42049
 function spawnRubbish()
     local x = math.random(xStart, xEnd)
     local y = -math.random(yStart, yEnd)
-    local pickup = createPickup(x, y ,z, 3, 2674, math.inf)
-    set
+    local pickup = createPickup(x, y ,z, 3, 2674, 5000)
+    table.insert(rubbish, pickup)
 end
 -- {2451.45044, -1628.72656, 228.42049}
 --  {2410.84326, -1665.77454, 228.42049}
 addEventHandler('onResourceStart', resourceRoot, function()
-    setTimer(function()
-        spawnRubbish()
-       outputDebugString('spawned rubbish!')
-    end, 1000, 5)
+    for i = 1,10 do
+        setTimer(spawnRubbish, math.random(3,10) * 1000, 1)
+    end
+    
+end)
+
+addEventHandler('onPlayerPickupHit', resourceRoot, function()
+    activeRubbish = activeRubbish - 1
+    if(activeRubbish < treshold) then
+        outputDebugString("Rubbish is okay")
+    end
+end)
+addEventHandler ( "onPickupSpawn", resourceRoot, function()
+    activeRubbish = activeRubbish + 1
+    if(activeRubbish >= treshold) then
+        outputDebugString("Rubbish is bad")
+    end
 end)
