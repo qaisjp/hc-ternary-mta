c3eb17c33a3c135f00b9663e668abc705a235406
Qais Patankar <qaisjp@gmail.com>
2018-01-21 01:12:21 +0000

    a.

--- game/server.lua
+++ game/server.lua
@@ -6,16 +6,16 @@ local gamestate = {
 -- Well Stacked Pizza Co spawnpoints
 -- Interior 5
 local spawnpoints = {
-    {379.55991, -115.55063, 1000.45},
-    {376.55991, -115.55063, 1000.45},
-    {373.55991, -115.55063, 1000.45}
+    {379.55991, -115.55063, 1001},
+    {377.55991, -115.55063, 1001},
+    {375.55991, -115.55063, 1001},
+    {373.55991, -115.55063, 1001}
 }
 
 -- { 379.58090209961, -120.8191986084, 1002.2612915039, 379.58090209961, -20.819198608398, 1002.2612915039, 0, 70 }
 local spawnIndex = math.random(#spawnpoints)
 
-local skins = {29, 194}
-local skinIndex = math.random(#skins)
+local skins = {29, 194, 46, 233, 250}
 
 addEventHandler('onResourceStart', resourceRoot, function()
     -- Load wifiObjects
@@ -29,14 +29,31 @@ addEventHandler('onResourceStart', resourceRoot, function()
     end
 end)
 
+function changeSkin(player, key)
+    local direction = (key == 'left') and -1 or 1
+    local index = direction + player:getElementData('spawn-select:skin')
+    if index < 1 or index > #skins then
+        index = (direction == 1) and 1 or #skins
+    end
+
+    player.model = skins[skinIndex]
+end
+
 addEvent('hc:onPlayerReady', true)
 addEventHandler('hc:onPlayerReady', root, function()
     outputChatBox('Welcome to Hackathon Simulator Cambridge.', source)
 
     local x, y, z = unpack(spawnpoints[spawnIndex])
+    local skinIndex = math.random(#skins)
     spawnPlayer(source, x, y, z, 180, skins[skinIndex], 5)
-    setCameraMatrix(source, x, y-5, z+2, x, y, z+2, 0, 70)
+    setCameraMatrix(source, x, y-5, z+1, x, y, z+1, 0, 70)
+    -- setPedFrozen(source, true)
+    toggleAllControls(source, false)
     fadeCamera(source, true)
+
+    setElementData(source, 'spawn-select:skin', skinIndex)
+    bindKey(source, "left", "up", changeSkin)
+    bindKey(source, "right", "up", changeSkin)
 
     spawnIndex = spawnIndex + 1
     skinIndex = skinIndex + 1
